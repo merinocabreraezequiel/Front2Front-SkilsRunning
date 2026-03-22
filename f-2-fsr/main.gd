5f2e515e9c38b299282f9db145f4c833a97343c3
Ezequiel Merino Cabrera <merinocabrera.ezequiel@gmail.com>
2026-03-22 19:39:06 +0100

    double screen works

--- f-2-fsr/main.gd
+++ f-2-fsr/main.gd
@@ -1,16 +1,22 @@
 extends Node2D
-
+@onready var view_1 = $HBoxContainer/SubViewportContainer/SubViewport
+@onready var view_2 = $HBoxContainer/Control_P2/SubViewportContainer2/SubViewport
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Jugador 1 ve el mundo normal
-	$HBoxContainer/SubViewportContainer/SubViewport/Camera2D.rotation_degrees = 0
+	$HBoxContainer/SubViewportContainer/SubViewport/Camera_P1.rotation_degrees = 0
 	# Jugador 2 ve el mundo invertido (mirando hacia el Jugador 1)
-	$HBoxContainer/SubViewportContainer2/SubViewport/Camera2D.rotation_degrees = 180
-
+	$HBoxContainer/Control_P2/SubViewportContainer2/SubViewport/Camera_P2.rotation_degrees = 180
+	
+	# El Viewport 2 compartirá el "mundo" (físicas, luces, objetos) del Viewport 1
+	# Nota: Usa .world_3d si tu juego es en 3D
+	view_2.world_2d = view_1.world_2d
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	$HBoxContainer/SubViewportContainer/SubViewport/Camera_P1.global_position = $HBoxContainer/SubViewportContainer/SubViewport/world_2d/Jugador1.global_position
+	$HBoxContainer/Control_P2/SubViewportContainer2/SubViewport/Camera_P2.global_position = $HBoxContainer/SubViewportContainer/SubViewport/world_2d/Jugador2.global_position
 	pass
 
 # Ejemplo rápido para detectar en qué lado se toca
